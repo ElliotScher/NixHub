529e6bf2bac71eedbc09e19bb0b51648f1dd9ced
ElliotScher <ecscher84@gmail.com>
2026-07-25 17:47:55 +0000

    Add host atta

---
+++ hosts/atta/home/elliotscher.nix
@@ -0,0 +1,7 @@
+{ config, pkgs, lib, inputs, ... }:
+
+{
+  # Host-specific home-manager overrides for elliotscher on atta go here.
+  # Anything in ../../../users/elliotscher/home.nix marked with lib.mkDefault can
+  # be overridden with a plain assignment.
+}
