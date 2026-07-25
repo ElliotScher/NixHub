289a3a2761930457cb03cda55d455527ce89f212
ElliotScher <ecscher84@gmail.com>
2026-07-25 17:50:24 +0000

    Revert "Add host atta"
    This reverts commit 529e6bf2bac71eedbc09e19bb0b51648f1dd9ced.

--- hosts/atta/configuration.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, lib, inputs, ... }:
-
-{
-  # Host-specific system overrides for atta go here.
-  # networking.hostName is set automatically from this directory's name.
-  # Anything in ../../common/configuration.nix marked with lib.mkDefault can
-  # be overridden with a plain assignment.
-}
